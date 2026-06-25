ca1f112b7de499a93326c6d6823c3be0498e292e
Aldair Vargas Mora <aldair@Aldairs-MacBook-Pro.local>
2026-06-25 11:46:17 -0600

    add next.config.js with ignoreBuildErrors

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"],
+  },
   images: {
     domains: ["avatars.slack-edge.com"],
   },
